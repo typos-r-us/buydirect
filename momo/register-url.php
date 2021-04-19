f6a409158f58f7e742a255f81370031eb65ec977
Kenneth Mwenda <kennethmwenda@gmail.com>
2021-04-19 16:10:13 +0300

    Add m-pesa transaction simulation and token generator

---
+++ momo/register-url.php
@@ -0,0 +1,29 @@
+ <?php
+  include './access-token.php'; #test if this works
+  $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
+  
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$accessToken)); //setting custom header
+  
+  
+  $curl_post_data = array(
+    //Fill in the request parameters with valid values
+    'ShortCode' => '174379',
+    'ResponseType' => 'Confirmed',
+    'ConfirmationURL' => 'https://buydirect.palacina.com/momo/confirmation-url.php',
+    'ValidationURL' => 'https://buydirect.palacina.com/momo/validation-url.php'
+  );
+  
+  $data_string = json_encode($curl_post_data);
+  
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+  
+  $curl_response = curl_exec($curl);
+  print_r($curl_response);
+  
+  echo $curl_response;
+  ?>
+  
